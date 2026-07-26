a3e7824e0c00134bb6324c6dabbbe890a5589ea7
Pincheng Wang <wpcwzy@outlook.com>
2026-07-26 12:25:34 +0800

    arch/riscv: add jump trampoline optimization

--- arch/riscv/scanner_riscv.c
+++ arch/riscv/scanner_riscv.c
@@ -936,6 +936,13 @@ size_t scan_riscv(dbm_thread *thread_data, uint16_t *read_address,
     write_p = (uint16_t *)&thread_data->code_cache->blocks[basic_block];
   }
 
+#ifdef DBM_JUMP_TRAMPOLINES
+  if (type == mambo_bb) {
+    thread_data->code_cache_meta[basic_block].trampoline_addr = NULL;
+    thread_data->code_cache_meta[basic_block].trampoline_slots_used = 0;
+  }
+#endif
+
   uint16_t const *start_address = write_p;
 
   uint32_t *data_p;
@@ -1474,6 +1481,13 @@ size_t scan_riscv(dbm_thread *thread_data, uint16_t *read_address,
                                 &write_p, &data_p, basic_block, type, false, &stop);
   riscv_scanner_deliver_callbacks(thread_data, POST_FRAGMENT_C, &start_scan, -1,
                                 &write_p, &data_p, basic_block, type, false, &stop);
+
+#ifdef DBM_JUMP_TRAMPOLINES
+  if (type == mambo_bb &&
+      (uintptr_t)data_p - (uintptr_t)write_p >= RISCV_TRAMPOLINE_SLOTS * 4) {
+    thread_data->code_cache_meta[basic_block].trampoline_addr = write_p;
+  }
+#endif
 
   return write_p - start_address;
 }
